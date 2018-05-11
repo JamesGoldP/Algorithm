a68b47ae9a750aafb751249b70b3650782f8dcd3
Nezumi <948800401@qq.com>
2018-05-11 15:17:51 +0800

    init

---
+++ App/Index/Controller/Index.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Index\Controller;
+
+use Zero\library\Controller;
+use Zero\library\Factory;
+
+class Index extends Controller
+{
+	public function index()
+	{
+		
+	}
+
+	public function show()
+	{
+	}
+
+	public function init()
+	{
+		
+	}	
+}
