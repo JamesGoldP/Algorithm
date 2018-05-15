98a601e70f0fd5cb3dca11b56498a241ccfbd728
Nezumi <948800401@qq.com>
2018-05-15 23:31:38 +0800

    Rewriting LinkedList.php.

--- App/Index/Model/LinkedList.php
+++ App/Index/Model/LinkedList.php
@@ -5,33 +5,25 @@ use Zero\library\Model;
 
 class LinkedList extends Model
 {
-    private $_firstNode = null;
-    private $_totalNode = 0;
+	public $_firstNode = NULL;
+	public $_totalNode = 0;
 
-    public function ainsert(string $data = null)
-    {
-        $newNode = new ListNode($data);
-        var_dump($newNode);
-        if ($this->_firstNode!==null) {
-            $currentNode = $this->_firstNode;
-            while ($currentNode->next !== null) {
-                $currentNode = $currentNode->next;
-            }
-            $currentNode->next = $newNode;
-        } else {
-        	$this->_firstNode = &$newNode;
-        }
-        $this->_totalNode++;
-        return true;
-    }
+	public function ainsert(string $data)
+	{
+		$newNode = new ListNode($data); 
+		if( $this->_firstNode !== NULL ){
+			$currentNode = $this->_firstNode;
+			while ( $currentNode->next !== NULL ) {
+				$currentNode = $currentNode->next;   //这个循环相当于$this->_firstNode->next->next->next,无限next,不能直接使用$this->_firstNode,$this->_firstNOde的值不能改变，它需要每次进来的时候赋值给$currentNode,然后循环得到最里面的next
+			}
+			$currentNode->next = $newNode;	
+		} else {
+			$this->_firstNode = $newNode;
+		}
+	}
 
-    public function adisplay()
-    {
-        echo 'Total book titles:'.$this->_totalNode.'<br/>';
-        $currentNode = $this->_firstNode;
-        while ($currentNode!==null) {
-            echo $currentNode->data.'<br/>';
-            $currentNode = $currentNode->next;
-        }
-    }
+	public function adisplay()
+	{
+		p($this->_firstNode);
+	}
 }
