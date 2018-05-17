9b04b2ed07031813c12862c3660a77d22278652a
Nezumi <948800401@qq.com>
2018-05-17 21:48:49 +0800

    Completing linked list operation

--- App/Index/Controller/DataStructures.php
+++ App/Index/Controller/DataStructures.php
@@ -9,50 +9,57 @@ use SplFixedArray;
 
 class DataStructures extends Controller
 {
-	public function stack()
-	{
-		$stack = new SplStack();
-		$stack->push('data1');
-		$stack->push('data2');
-		$stack->push('data3');	
-		
-		echo $stack->pop(); 
-		//先进后出，后进先出	
-	}
+    public function stack()
+    {
+        $stack = new SplStack();
+        $stack->push('data1');
+        $stack->push('data2');
+        $stack->push('data3');
+        
+        echo $stack->pop();
+        //先进后出，后进先出
+    }
 
-	public function quene()
-	{
-		$queue = new SplQueue();
-		$queue->enqueue('data1');	
-		$queue->enqueue('data2');	
-		$queue->enqueue('data3');
+    public function quene()
+    {
+        $queue = new SplQueue();
+        $queue->enqueue('data1');
+        $queue->enqueue('data2');
+        $queue->enqueue('data3');
 
-		echo $queue->dequeue();
-		//先进先出，后进后出	
-	}	
+        echo $queue->dequeue();
+        //先进先出，后进后出
+    }
 
-	public function minheap()
-	{
-		//最小堆
+    public function minheap()
+    {
+        //最小堆
+    }
 
-	}
+    public function fixedarray()
+    {
+        //固定长度的数组
+        $array = new SplFixedArray(10);
+        $array[0] = 123;
+        $array[9] = 1234;
+        p($array);
+    }
 
-	public function fixedarray()
-	{
-		//固定长度的数组
-		$array = new SplFixedArray(10);
-		$array[0] = 123;
-		$array[9] = 1234;
-		p($array);
-	}
-
-	public function display_linkedlist()
-	{
-		$linkedlist = Factory::getModel('LinkedList');
-		$linkedlist->ainsert('Introduction to Algorithum');	
-		$linkedlist->ainsert('Introduction to PHP and Data structures');
-		$linkedlist->ainsert('Programming Intelligence');
-		$linkedlist->adisplay();
-	}	
-
+    public function display_linkedlist()
+    {
+        $BookTitles = Factory::getModel('LinkedList');
+        $BookTitles-> insert("Introduction to Algorithm");
+        $BookTitles-> insert("Introduction to PHP and Data structures");
+        $BookTitles-> insert("Programming Intelligence");
+        $BookTitles-> insertAtFirst("Mediawiki Administrative tutorial guide");
+        $BookTitles-> insertBefore("Introduction to Calculus", "Programming Intelligence");
+        $BookTitles-> insertAfter("Introduction to Calculus", "Programming Intelligence");
+        p($BookTitles-> display());
+        $BookTitles-> deleteFirst();
+        $BookTitles-> deleteLast();
+        $BookTitles-> delete("Introduction to PHP and Data structures");
+        $BookTitles-> reverse();
+        p($BookTitles-> display());
+        echo "2nd Item is: ". $BookTitles-> getNthNode(2)-> data;
+    }
 }
--- App/Index/Model/LinkedList.php
+++ App/Index/Model/LinkedList.php
@@ -5,25 +5,260 @@ use Zero\library\Model;
 
 class LinkedList extends Model
 {
-	public $_firstNode = NULL;
-	public $_totalNode = 0;
+	private $_firstNode = NULL;
+	private $_totalNode = 0;
 
-	public function ainsert(string $data)
+	/**
+	 * Whether is empty 
+	 * @return boolean
+	 */ 	
+	public function isEmpty()
 	{
-		$newNode = new ListNode($data); 
-		if( $this->_firstNode !== NULL ){
+		return $this->_firstNode === NULL;		
+	}
+
+	/**
+	 * Getting the size of the linked list 
+	 * 
+	 */ 	
+	public function getTotal()
+	{
+		return $this->_totalNode;		
+	}
+
+	/**
+	 * Inserting a new item to the linkedlist.
+	 * 
+	 */
+	public function insert(string $data = NULL)
+	{
+		if( !$this->isEmpty() ){
 			$currentNode = $this->_firstNode;
-			while ( $currentNode->next !== NULL ) {
-				$currentNode = $currentNode->next;   //这个循环相当于$this->_firstNode->next->next->next,无限next,不能直接使用$this->_firstNode,$this->_firstNOde的值不能改变，它需要每次进来的时候赋值给$currentNode,然后循环得到最里面的next
+			while( $currentNode->next !== NULL ){		
+				$currentNode = $currentNode->next;
 			}
-			$currentNode->next = $newNode;	
+			$currentNode->next = $this->createNode($data);	
 		} else {
-			$this->_firstNode = $newNode;
+			$this->insertAtFirst($data);	
+		}
+		return true;	
+	}
+
+	/**
+	 * Display all items in the linked list.
+	 * 
+	 */
+	public function display()
+	{
+		$string = 'Total:'.$this->_totalNode.'<br/>';
+		if( !$this->isEmpty() ){
+			$currentNode = $this->_firstNode;
+			while( $currentNode !== NULL ){	
+				$string.= $currentNode->data.'<br/>';	
+				$currentNode = $currentNode->next;
+			}
+		} 	
+		return $string;
+	}
+
+	/**
+	 * Inserting a new item at the first.
+	 * 
+	 */
+	public function insertAtFirst(string $data = NULL)
+	{
+		$newNode = $this->createNode($data);
+		if( $this->_firstNode === NULL){
+			$this->_firstNode = $newNode;	
+		} else {
+			$newNode->next = $this->_firstNode;
+			$this->_firstNode = $newNode;	
+		}
+		return true;	
+	}
+
+	private function createNode(string $data = NULL)
+	{
+		$this->_totalNode++;
+		return new ListNode($data);
+	}
+
+	/**
+	 * Inserting a new item at end of the linked list.
+	 * 
+	 */
+	public function search( string $query = NULL )
+	{
+
+		if( !$this->isEmpty() ){
+			$currentNode = $this->_firstNode;
+			while ( $currentNode !== NULL ) {
+				if( $currentNode->data == $query) {
+					return $currentNode;  
+				}
+				$currentNode = $currentNode->next;
+			}
+
+		}	
+		return FALSE;
+	}
+
+	/**
+	 * 
+	 * Getting the Nth item.
+	 * 
+	 */
+	public function getNthNode(int $n = 0)
+	{
+		if( !$this->isEmpty() ){
+			$currentNode = $this->_firstNode;
+			$i = 1;
+			while ( $currentNode !== NULL ) {
+				if( $n == $i ) {
+					return $currentNode;  
+					break;	
+				}
+				$currentNode = $currentNode->next;
+				$i++;
+			}
+		}	
+	}
+
+
+	/**
+	 * Inserting a new item after a specific tiem.
+	 * 
+	 */
+	public function insertAfter(string $data = NULL, string $query = NULL)
+	{
+		if( !$this->isEmpty() ){
+			$newNode = $this->createNode($data);
+			$currentNode = $this->_firstNode;
+			while ( $currentNode !== NULL ) {
+				if( $currentNode->data === $query) {
+					$newNode->next = $currentNode->next; 
+					$currentNode->next = $newNode;
+					break;	
+				}
+				$currentNode = $currentNode->next;
+			}
+		}	
+
+	}
+
+
+	/**
+	 * Inserting a new item before a specific tiem.
+	 * 
+	 */
+	public function insertBefore(string $data = NULL, string $query = NULL)
+	{
+		if( !$this->isEmpty() ){
+			$currentNode = $this->_firstNode;
+			$previous = NULL;
+			$newNode = $this->createNode($data);
+			while ( $currentNode !== NULL ) {
+				if( $currentNode->data === $query) {
+					$newNode->next = $currentNode;
+					$previous->next = $newNode;
+					break;	
+				}
+				$previous = $currentNode;
+				$currentNode = $currentNode->next; // $currentNode->netxt这里的$currentNode是前一个
+			}
+		}	
+
+	}
+
+	/**
+	 * 
+	 * Deleting the first item.
+	 * 
+	 */
+	public function deleteFirst()
+	{
+		
+		if( !$this->isEmpty() ){
+			if( $this->_firstNode->next !== NULL ){
+				$this->_firstNode = $this->_firstNode->next;
+			} else {
+				$this->_firstNode = NULL;
+			}
+			$this->_totalNode--;
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	/**
+	 * 
+	 * Deleting the last item.
+	 * 
+	 */
+	public function deleteLast()
+	{
+		
+		if( !$this->isEmpty() && $this->_firstNode->next !== NULL){
+			$previous = NULL;
+			$currentNode = $this->_firstNode;
+			while( $currentNode->next !== NULL ){	
+				$previous = $currentNode;	
+				$currentNode = $currentNode->next;
+			}
+			$previous->next = NULL;
+			$this->_totalNode--;
+			return TRUE;
+		} else {
+			$this->_firstNode = NULL;
 		}
 	}
 
-	public function adisplay()
+	/**
+	 * 
+	 * Deleting an item which is searched for .
+	 * 
+	 */
+	public function delete(string $query)
 	{
-		p($this->_firstNode);
+		
+		if( !$this->isEmpty() ){
+			$currentNode = $this->_firstNode;
+			$previous = NULL;
+			while ( $currentNode !== NULL ) {
+				if( $currentNode->data == $query) {
+					if( $currentNode->next !== NULL ){
+						$previous->next = $currentNode->next; 
+					} else {
+						$previous->next = NULL;
+					}
+					$this->_totalNode--;
+					break;
+				}
+				$previous = $currentNode;
+				$currentNode = $currentNode->next;
+			}
+		}			
 	}
+
+	/**
+	 * 
+	 * Reversing the linked list .
+	 * 
+	 */
+	public function reverse()
+	{
+		if( !$this->isEmpty() && $this->_firstNode->next !== NULL){
+			$reversedList = NULL;	
+			$currentNode = $this->_firstNode;
+			while ( $currentNode !== NULL ) {
+				$nextNode = $currentNode->next;
+				$currentNode->next = $reversedList;
+				$reversedList = $currentNode;
+				$currentNode = $nextNode;
+			}
+			$this->_firstNode = $reversedList;
+		}
+	}
+
+
 }
